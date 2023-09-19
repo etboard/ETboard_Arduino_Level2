0941fa64cd6a07dc9dc2e47fca641809cdc8439a
kpej <126046230+kpej@users.noreply.github.com>
2023-09-19 17:27:15 +0900

    Edit Servo, Button pin

--- src/21_servo_motor/03._servo_motor_up_down/03._servo_motor_up_down.ino
+++ src/21_servo_motor/03._servo_motor_up_down/03._servo_motor_up_down.ino
@@ -1,18 +1,19 @@
 /******************************************************************************************
  * FileName     : 03._servo_motor_up_down
- * Description  : 빨강, 노랑 버튼을 눌러 서보모터 움직여 보기
+ * Description  : 파랑, 초록 버튼을 눌러 서보모터 움직여 보기
  * Author       : 이인정
  * Created Date : 2021.06.08
  * Reference    : 
  * Modified     : 2021.06.08 : LIJ : 헤더 수정 
  * Modified     : 2021.12.16 : LEH : 소스 크린징
+ * Modified     : 2023.9.19 : PEJ : 서보모터, 버튼 핀 변경
 ******************************************************************************************/
 
 #include <Servo.h>
 
-static const int servoPin = D2;             // 서보모터
-int up = D6;                                // 빨강 버튼         
-int down = D9;                              // 노랑 버튼
+static const int servoPin = D6;             // 서보모터
+int up = D7;                                // 파랑 버튼         
+int down = D8;                              // 초록 버튼
 
 Servo servo;
 
@@ -26,14 +27,14 @@ void setup() {
 
 
 void loop() {
-  int up_value = digitalRead(up);           // 빨강 버튼 상태확인
-  int down_value = digitalRead(down);       // 노랑 버튼 상태확인
+  int up_value = digitalRead(up);           // 파랑 버튼 상태확인
+  int down_value = digitalRead(down);       // 초록 버튼 상태확인
   
-  if(up_value == LOW){                      // 빨강 버튼이 눌리면 서보모터 180도까지 회전
+  if(up_value == LOW){                      // 파랑 버튼이 눌리면 서보모터 180도까지 회전
     servo.write(180);
   }
   
-  if(down_value == LOW){                    // 노랑 버튼이 눌리면 서보모터 0도까지 회전
+  if(down_value == LOW){                    // 초록 버튼이 눌리면 서보모터 0도까지 회전
     servo.write(0);
   }
 }
