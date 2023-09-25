a087c717a2dbbd87bbb9665f9832e56489296e16
kpej <126046230+kpej@users.noreply.github.com>
2023-09-25 16:51:40 +0900

    Fix bug

--- src/52_ultrasonic_buzzer/ultrasonic_buzzer/ultrasonic_buzzer.ino
+++ src/52_ultrasonic_buzzer/ultrasonic_buzzer/ultrasonic_buzzer.ino
@@ -6,26 +6,24 @@
  * Reference    : 
  * Modified     : 2021.06.18 : LIJ : 헤더 수정
  * Modified     : 2021.12.16 : LEH : 소스 크린징
+ * Modified     : 2023.09.25 : PEJ : 소스 코드 오류 수정
 ******************************************************************************************/
 
-#define TRIG D9                        // TRIG 핀 설정 (초음파 보내는 핀)
-#define ECHO D8                        // ECHO 핀 설정 (초음파 받는 핀)
+int TRIG = D9;                          // TRIG 핀 설정 (초음파 보내는 핀)
+int ECHO = D8;                          // ECHO 핀 설정 (초음파 받는 핀)
 
-int buzzer = D6 ;                      // 부저
-int i;
+int buzzer = D6 ;                       // 부저
 
 
-void setup()
-{
-  Serial.begin(115200);                  // 통신속도설정
-  pinMode(TRIG, OUTPUT);               // 핀 모드 설정
-  pinMode(ECHO, INPUT);                // 핀 모드 설정
-  pinMode (buzzer, OUTPUT) ;           // 핀 모드 설정
+void setup() {
+  Serial.begin(115200);                 // 통신 속도
+  pinMode(TRIG, OUTPUT);                // 핀 모드 설정
+  pinMode(ECHO, INPUT);                 // 핀 모드 설정
+  pinMode (buzzer, OUTPUT) ;            // 핀 모드 설정
 }
 
 
-void loop()
-{
+void loop() {
   long duration, distance;
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
@@ -33,14 +31,17 @@ void loop()
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
   
-  duration = pulseIn (ECHO, HIGH);     // 초음파가 돌아온시간 저장
-  distance = duration * 17 / 1000;     // 거리를 계산
-      
-  Serial.print(distance);              // 계산한 거리를 출력
-  if(distance<15)
-  {
-    for (i=0;i<80;i++)                 // 부저에 소리내기
-    {
+  duration = pulseIn (ECHO, HIGH);     // 물체에 반사되어 돌아온 초음파의 시간을 저장
+  
+  distance = duration * 17 / 1000;     // 측정된 값을 Cm로 변환하기 위해 공식을 대입
+  
+  Serial.print(distance);              // 측정된 물체로부터 거리를 시리얼 모니터로 출력
+  Serial.println(" Cm");
+
+  delay(100);                          // 0.1초 대기
+  
+  if(distance < 15) {
+    for (i = 0; i < 80; i++) {         // 부저에 소리 내기
       digitalWrite(buzzer, HIGH);
       delay(1);
       digitalWrite(buzzer, LOW);
